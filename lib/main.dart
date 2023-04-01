84aebb6e8d315cbbe41676be34883e4a54193641
Messi002 <rostandmessi2@gmail.com>
2023-04-01 04:43:19 +0100

    refactoring...

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidge
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
   
         // is not restarted.
         primarySwatch: Colors.blue,
