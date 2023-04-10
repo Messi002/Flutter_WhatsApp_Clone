f8927efa0a594180ecbe85e4c47d6ea80386c6e6
Messi002 <rostandmessi2@gmail.com>
2023-04-10 01:39:25 +0100

    added authController logic

--- lib/features/auth/controllers/auth_controller.dart
+++ lib/features/auth/controllers/auth_controller.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/features/auth/repository/auth_repository.dart';
+
+final authControllerProvider = Provider((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AuthController(authRepository: authRepository);
+});
 
 class AuthController {
   final AuthRepository authRepository;
 
   AuthController({required this.authRepository});
 
-  void signInWithPhone(BuildContext context, String phone){
-    
+  void signInWithPhone(BuildContext context, String phoneNumber) {
+    authRepository.signInWithPhone(context, phoneNumber);
   }
 }
